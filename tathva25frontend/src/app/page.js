3d5d44c2ac8bdc93752a47bd10cff9b5ef5eb9d8
Emir Ziyan <emirziyan@gmail.com>
2025-10-10 21:02:20 +0530

    made a sidebar

--- tathva25frontend/src/app/page.js
+++ tathva25frontend/src/app/page.js
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import { Hero } from "@/components/hero";
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <main>
+
         <Hero />
+        <Sidebar />
       </main>
     </div>
-    
+
   );
 }
