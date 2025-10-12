9330c18a8e7394d0e9b758228a76a43204e89bc8
Abhinav Mathew <amonline2005@gmail.com>
2025-10-12 15:37:20 +0530

    robopage nearly completed

---
+++ app/components/Robowars.jsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import { Space_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
+/*FONTS*/
+const spacemono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-space-mono",
+});
+const schabo = localFont({
+  src: "../../public/fonts/schabo.woff2",
+});
+
+/*LINES*/
+function Line() {
+  return (
+    <>
+      <div className="flex justify-center mx-2 my-6 items-center">
+        <div className="bg-black w-[80%] rounded-full h-[6px]"></div>
+      </div>
+    </>
+  );
+}
+
+/*TEXTS*/
+function Region() {
+  return (
+    <>
+      <div className="mt-5 py-4">
+        <div className="flex justify-between flex-wrap px-4 sm:px-8 md:px-12 lg:px-20">
+          <div className="flex flex-col gap-3 sm:gap-4 md:gap-6">
+            <div>
+              <div className="flex gap-2 sm:gap-3 md:gap-5 flex-wrap items-center">
+                <div
+                  className={`bg-black px-2 pr-6 sm:pr-8 md:pr-16 py-1 ${schabo.className}`}
+                >
+                  <span className="text-white robopagetitle">OCT 26,27</span>
+                </div>
+                <div className={`bg-black py-1 ${schabo.className}`}>
+                  <span className="robopagetitle">l</span>
+                </div>
+
+                <div
+                  className={`${spacemono.className} text-[10px] sm:text-sm md:text-lg lg:text-xl text-[400] flex flex-col`}
+                >
+                  <span>30 x 30 FT. ARENA</span>
+                  <span>15KG \ 60KG</span>
+                </div>
+              </div>
+            </div>
+            <div className={`text-black ${schabo.className}`}>
+              <span className="robopagetitle">PRIZES WORTH INR 8 LAKH</span>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+/*Direct img tag to next Image tag converter..*/
+function ImgtoImage({ src, alt }) {
+  return (
+    <Image
+      src={`${src}`}
+      alt={`${alt}`}
+      width={0}
+      height={0}
+      sizes="100vw"
+      className="w-full h-auto object-cover"
+      style={{
+        width: "100%",
+        height: "auto",
+      }}
+    />
+  );
+}
+
+/*Main pic*/
+function Picture() {
+  return (
+    <div className="w-full">
+      <div className="w-full">
+        <ImgtoImage src="/robowars.png" alt="robowars" />
+      </div>
+    </div>
+  );
+}
+
+export default function Robowars({ link }) {
+  return (
+    <>
+      <div className="relative bg-[url('/robopagebg.png')] bg-cover">
+        <Line />
+        <Picture />
+        <Region />
+        <Line />
+      </div>
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Menupage from "./Menupage";
+import Robowars from "./components/Robowars";
 
 export default function Home() {
   return (
     <>
-      <Menupage />
+      <Robowars />
     </>
   );
 }
