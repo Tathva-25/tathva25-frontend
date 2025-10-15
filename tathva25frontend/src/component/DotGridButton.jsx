999ca0a4330cc716a9d7c3d4b0d452426d2cc6dd
r7thu <rithuu4@gmail.com>
2025-10-16 05:09:59 +0530

    font ready responsive

--- tathva25frontend/src/component/DotGridButton.jsx
+++ tathva25frontend/src/component/DotGridButton.jsx
@@ -2,6 +2,12 @@
 
 import DotGrid from "@/components/DotGrid";
 import React from "react";
+import { Michroma } from 'next/font/google';
+
+const michroma = Michroma({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function DotGridButton({
   text = "Experience It",
@@ -12,26 +18,27 @@ export default function DotGridButton({
       {/* DotGrid background */}
       <div className="absolute inset-0 bg-black -inset-x-10 -inset-y-2">
         <DotGrid
-          dotSize={2}
+          dotSize={2.3}
           gap={5}
           baseColor="#524d06"
-          activeColor="#c4b702"
+          activeColor="#ffee00"
           proximity={50}
         />
       </div>
 
       {/* Button foreground */}
       <button
-        className="
+        className={`
           relative 
           px-10 
           py-2
           text-white 
           font-semibold 
           bg-transparent
+          ${michroma.className}
           z-10 
           pointer-events-auto
-        "
+        `}
       >
         {text}
       </button>
