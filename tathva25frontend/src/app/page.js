5c716c1a5e72aa2c5beda53771f33eeca60c14d6
Abhinav Mathew <amonline2005@gmail.com>
2025-10-17 00:32:44 +0530

    ok for desk

--- tathva25frontend/src/app/page.js
+++ tathva25frontend/src/app/page.js
@@ -1,21 +1,12 @@
 import Image from "next/image";
 import Menupage from "./components/Menupage";
 import Robowars from "./components/Robowars";
+import RB1 from "./components/RB1";
 
 export default function Home() {
   return (
     <>
-      {/* Sci-fi Background Image */}
-      <div
-        className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat"
-        style={{
-          backgroundImage:
-            "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe3ZPFCxCFME9-1GCbHwAkv8bcPtpC1z_Fdg&s')",
-        }}
-      >
-        <div className="absolute inset-0 bg-black/50"></div>
-      </div>
-      <Menupage />
+      <RB1 />
     </>
   );
 }
