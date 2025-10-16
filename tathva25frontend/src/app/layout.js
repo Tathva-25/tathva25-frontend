8b3cc554a946407157d6dee086f51c00bfa630e4
1-Nishanth-1 <nishanthrn1@gmail.com>
2025-10-16 21:32:19 +0530

    changes

--- tathva25frontend/src/app/layout.js
+++ tathva25frontend/src/app/layout.js
@@ -1,14 +1,10 @@
 import "./globals.css";
-import MenuWrapper from "./components/MenuWrapper";
-
 export const metadata = {
   title: "Tathva 25",
   description: "Tathva",
 };
 
 export default function RootLayout({ children }) {
-  const [showSplash, setShowSplash] = useState(true);
-
   return (
     <html lang="en">
       <body className="relative overflow-x-hidden">
