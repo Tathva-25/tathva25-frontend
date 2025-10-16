c17c2cb779d5da2b6f8b172c762d851b401cdb24
Rahan-M <rahan01729@gmail.com>
2025-10-17 00:26:37 +0530

    feat: adding text

--- tathva25frontend/src/app/gaming/page.jsx
+++ tathva25frontend/src/app/gaming/page.jsx
@@ -1,14 +1,21 @@
 import Image from "next/image"
 import localFont from "next/font/local"
+import Sidebar from "@/components/sidebar"
 import gaming_hero from "../../../public/images/gaming/gaming_hero.png"
-import text_box_1 from "../../../public/images/gaming/text_box_1.png"
-import text_box_2 from "../../../public/images/gaming/text_box_2.png"
+import { Michroma } from "next/font/google"
+import DotGridButton from "@/components/DotGridButton"
 import { JetBrains_Mono } from 'next/font/google'
 import effectStyles from "./gaming_styles.module.css"
+import { Mic } from "lucide-react"
 
 export const integralCF = localFont({
   // The files you're pointing to are relative to the project root
   src: [
+    {
+      path: '../../../public/fonts/Integral-CF/Demo_Fonts/Fontspring-DEMO-integralcf-medium.otf',
+      weight: '200',
+      style: 'normal',
+    },
     {
       path: '../../../public/fonts/Integral-CF/Demo_Fonts/Fontspring-DEMO-integralcf-regular.otf',
       weight: '400',
@@ -42,11 +49,12 @@ const jetBrainsMono = JetBrains_Mono({
 const GamePage = () => {
   return (
     <div className="bg-white flex flex-col sm:flex-row items-center justify-center relative">
+      <Sidebar/>
       {/* <Sidebar /> */}
         {/* Background NEXUS text with hexagons */}
         <div className="absolute inset-0 pointer-events-none flex flex-col justify-center items-center text-[7rem] leading-[0.9] sm:text-[12rem] md:text-[25rem] sm:leading-[0.9] sm:tracking-widest">
           {/* Add the new class here */}
-          <div className={`${effectStyles.effectContainer} ${integralCF.className} font-bold h-full flex flex-col justify-between`}
+          <div className={`${effectStyles.effectContainer} ${integralCF.className} font-bold h-full flex flex-col justify-between pb-10`}
             style={{
               WebkitTextStroke: '2px black', 
             }}
@@ -77,34 +85,13 @@ const GamePage = () => {
             </div>
         </div>
 
-        <div className="sm:py-5 lg:px-10 lg:w-[40%] flex sm:justify-center sm:items-center relative z-10 ">
-          <div className="relative w-full sm:h-screen flex items-center justify-center p-4 ">
-            <Image
-              src={text_box_1}
-              // width={350}
-              alt="textbox_1"
-              className="sm:w-[30rem] lg:w-[30rem]"
-            />
-            <div className="absolute text-center">
-              <div className={`${jetBrainsMono.className} text-gray-800 text-lg leading-relaxed font-mono mb-6`}>
-                <p>Show off your skills and</p>
-                <p>conquer the arena at <span className="text-red-800">Gaming</span></p>
-                <p><span className="text-red-800">Conclave</span>, where only the</p>
-                <p>best rise to the top</p>
+        <div className="lg:px-10 lg:w-[40%] flex sm:justify-center sm:items-center relative z-10 ">
+              <div className={`${Michroma.} text-lg leading-relaxed mb-6`}>
+                Show off your skills and
+                conquer the arena at <span className="text-red-800">Gaming</span>
+                <span className="text-red-800">Conclave</span>, where only the
+                best rise to the top
               </div>
-            </div>
-            <div className="absolute translate-y-20">
-              <div className="relative flex items-center justify-center gap-3">
-                <Image
-                  src={text_box_2}
-                  alt="text_box_2"
-                  className="w-[20rem]"
-                  // width={300}
-                />
-                <p className="absolute text-[1rem]">LEARN MORE</p>
-              </div>
-            </div>
-          </div>
         </div>
     </div>
   )
