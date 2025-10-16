c4cee1cf15c7932e36e3b1195ae642a019b64535
Rahan-M <rahan01729@gmail.com>
2025-10-17 04:20:00 +0530

    feat: gaming concalve done

--- tathva25frontend/src/app/gaming/page.jsx
+++ tathva25frontend/src/app/gaming/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import localFont from "next/font/local"
 import Sidebar from "@/components/sidebar"
 import gaming_hero from "../../../public/images/gaming/gaming_hero.png"
+import gaming_hero_mobile from "../../../public/images/gaming/gaming_hero_mobile.png"
 import { Michroma } from "next/font/google"
 import DotGridButton from "@/components/DotGridButton"
 import { JetBrains_Mono } from 'next/font/google'
@@ -26,6 +27,11 @@ export const integralCF = localFont({
       weight: '700',
       style: 'normal',
     },
+    {
+      path: '../../../public/fonts/Integral-CF/Demo_Fonts/Fontspring-DEMO-integralcf-extrabold.otf',
+      weight: '800',
+      style: 'normal',
+    }
     // Add other weights/styles as needed
   ],
   variable: '--font-integral-cf', // Optional: for using with CSS variables
@@ -50,58 +56,99 @@ const michroma = Michroma({ subsets: ["latin"], weight: "400" })
 
 const GamePage = () => {
   return (
-    <div className="bg-white flex flex-col-reverse sm:flex-row items-center justify-center relative">
-      {/* <Sidebar/> */}
-      {/* <Sidebar /> */}
-        {/* Background NEXUS text with hexagons */}
-        <div className="absolute inset-0 pointer-events-none flex flex-col justify-center items-center text-[7rem] leading-[0.9] sm:text-[12rem] md:text-[25rem] sm:leading-[0.9] sm:tracking-widest">
+    
+    <div>
+      <div className="hidden lg:flex bg-white flex-row items-center justify-center relative overflow-x-hidden min-h-screen">
+        {/* <Sidebar/> */}
+        {/* <Sidebar /> */}
+          {/* Background NEXUS text with hexagons */}
+          <div className="absolute inset-0 pointer-events-none flex flex-col justify-center items-center text-[25rem] leading-[0.9] tracking-widest">
+            {/* Add the new class here */}
+            <div className={`${effectStyles.effectContainer} ${integralCF.className} font-bold h-full flex flex-col justify-between items-center pb-10`}
+              style={{
+                WebkitTextStroke: '2px black', 
+              }}
+            >
+              <div>NEXUS</div>
+              <div>NEXUS</div>
+            </div>
+          </div>
+
+          <div className="z-10 flex flex-col items-center h-screen justify-center translate-x-40"> 
+              {/* mx-auto centers the 60% container horizontally on the page */}
+              {/* flex-col stacks children vertically */}
+              {/* items-center centers children (Image container and Text container) horizontally */}
+
+              {/* Image Container: Added flex justify-center to horizontally center the Image inside this 100% width container */}
+              <div className="h-screen">
+                  <Image
+                      src={gaming_hero}
+                      // width={500}
+                      alt="Hero_Image"
+                      className="h-screen w-auto object-cover"
+                      priority
+                  />
+              </div>
+          </div>
+
+          <div className="px-10 flex justify-center items-center relative z-10">
+            <div className="mr-15">
+              <div className={`${integralCF.className} text-[6vw] font-bold w-[40vw] text-center`}>
+                GPC NEXUS
+              </div>
+              <div className="text-right -translate-x-17 mt-3">
+                <DotGridButton text="Learn More" min_height={20} min_width={40}/>
+              </div>
+            </div>  
+            <div className={`${michroma.className} text-2xs text-xl leading-relaxed mb-6 mr-15 max-w-[25vw]`}>
+              Show off your skills and
+              conquer the arena at <span className="text-red-800">Gaming</span>
+              <span className="text-red-800">Conclave</span>, where only the
+              best rise to the top
+            </div>
+          </div>
+      </div>
+
+      {/* Phone Componenet */}
+      <div className="lg:hidden bg-gradient-to-r from-white to-[#d8d8d1] h-screen relative overflow-hidden flex flex-col justify-center px-3">
+        <div className="absolute inset-0 pointer-events-none flex flex-col justify-center items-center text-[25vw] leading-[1.1] ">
           {/* Add the new class here */}
-          <div className={`${effectStyles.effectContainer} ${integralCF.className} font-bold h-full flex flex-col justify-center sm:justify-between items-center pb-10`}
+          <div className={`${effectStyles.effectContainer} ${integralCF.className} font-extrabold flex flex-col justify-between items-center`}
             style={{
-              WebkitTextStroke: '2px black', 
+              WebkitTextStroke: '1px #8d705d', 
             }}
           >
             <div>NEXUS</div>
             <div>NEXUS</div>
-            <div className="sm:hidden">NEXUS</div>
-            <div className="sm:hidden">NEXUS</div>
-            <div className="sm:hidden">NEXUS</div>
-            <div className="sm:hidden">NEXUS</div>
+            <div>NEXUS</div>
+            <div>NEXUS</div>
+            <div>NEXUS</div>
+            <div>NEXUS</div>
           </div>
         </div>
-
-        <div className="z-10 flex flex-col items-center sm:h-screen justify-center sm:translate-x-40"> 
-            {/* mx-auto centers the 60% container horizontally on the page */}
-            {/* flex-col stacks children vertically */}
-            {/* items-center centers children (Image container and Text container) horizontally */}
-
-            {/* Image Container: Added flex justify-center to horizontally center the Image inside this 100% width container */}
-            <div className="sm:h-screen flex justify-center">
-                <Image
-                    src={gaming_hero}
-                    // width={500}
-                    alt="Hero_Image"
-                    className="sm:h-screen w-auto object-cover"
-                />
+        <div className="textbox relative z-10">
+            <div className={`${michroma.className} text-xs my-0 translate-y-5`}>Tathva25</div>
+            <div className={`text-[12vw] ${integralCF.className} font-bold`}>GPC NEXUS</div>
+            <div className={`${michroma.className} leading-relaxed text-[2vw] w-[70%]`}>
+              Show off your skills and
+              conquer the arena at <span className="text-red-800">Gaming</span>
+              <span className="text-red-800">Conclave</span>, where only the
+              best rise to the top
             </div>
         </div>
-
-        <div className="sm:px-10 flex flex-col sm:flex-row justify-center sm:items-center relative z-10">
-          <div className="sm:mr-15">
-            <div className={`${integralCF.className} text-xg sm:text-8xl font-bold text-left sm:w-[40rem]`}>
-              GPC NEXUS
-            </div>
-            <div className="absolute sm:static bottom-0 left-0 sm:text-right sm:-translate-x-17 mt-3">
-              <DotGridButton text="Learn More" min_height={20} min_width={40}/>
-            </div>
-          </div>  
-          <div className={`${michroma.className} text-2xs sm:text-xl leading-relaxed mb-6 sm:mr-15 sm:max-w-[25vw]`}>
-            Show off your skills and
-            conquer the arena at <span className="text-red-800">Gaming</span>
-            <span className="text-red-800">Conclave</span>, where only the
-            best rise to the top
-          </div>
+        <div className="relative z-10 w-full -translate-y-10 flex justify-center items-center">
+          <Image
+              src={gaming_hero_mobile}
+              // width={500}
+              alt="Hero_Image"
+              className=""
+              priority
+          />
         </div>
+        <div className="text-right absolute bottom-5 right-13">
+          <DotGridButton text="Learn More" min_height={20} min_width={20}/>
+        </div>
+      </div>
     </div>
   )
 }
