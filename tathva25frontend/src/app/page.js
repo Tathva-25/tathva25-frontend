b3af298dc84a0a5dae10de41954e56cfdc277a6f
Richie James <richiejames955@gmail.com>
2025-10-17 03:07:12 +0530

    merged workingSidebar to main

--- tathva25frontend/src/app/page.js
+++ tathva25frontend/src/app/page.js
@@ -1,23 +1,11 @@
 "use client";
-import Image from "next/image";
-import { Hero } from "@/components/heroPage/hero";
 import Navbar from "@/components/heroPage/navbar";
 import Sidebar from "@/components/heroPage/sidebar";
-import ResponsiveLayout from "@/components/Wheels";
-import Explore from "./components/Explore";
-import Menupage from "./components/Menupage";
-import Robowars from "./components/Robowars";
-
-import Proshow from "@/components/proshow";
 export default function Home() {
   return (
     <div>
-      {/* <Navbar /> */}
-        {/* <Hero />
-        <Explore/>
-        <Proshow/>
-        <ResponsiveLayout/> */}
-        <Sidebar />
+      <Navbar />
+      <Sidebar />
     </div>
   );
 }
