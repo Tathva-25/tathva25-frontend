3cefb30a7c15a296b92cd5453a7b547ed9ce67dc
Rahan-M <rahan01729@gmail.com>
2025-10-17 00:02:38 +0530

    fix: animation

--- tathva25frontend/src/app/gaming/page.jsx
+++ tathva25frontend/src/app/gaming/page.jsx
@@ -1,13 +1,28 @@
 import Image from "next/image"
-import localfont from "next/font/local"
+import localFont from "next/font/local"
 import gaming_hero from "../../../public/images/gaming/gaming_hero.png"
 import text_box_1 from "../../../public/images/gaming/text_box_1.png"
 import text_box_2 from "../../../public/images/gaming/text_box_2.png"
 import { JetBrains_Mono } from 'next/font/google'
+import effectStyles from "./gaming_styles.module.css"
 
-const customFont = localfont({
-    src: '../../../public/fonts/neoform.otf',
-})
+export const integralCF = localFont({
+  // The files you're pointing to are relative to the project root
+  src: [
+    {
+      path: '../../../public/fonts/Integral-CF/Demo_Fonts/Fontspring-DEMO-integralcf-regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../../public/fonts/Integral-CF/Demo_Fonts/Fontspring-DEMO-integralcf-bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    // Add other weights/styles as needed
+  ],
+  variable: '--font-integral-cf', // Optional: for using with CSS variables
+});
 
 const jetBrainsMono = JetBrains_Mono({
   // Specify which subsets you need to load (latin is the default)
@@ -26,12 +41,16 @@ const jetBrainsMono = JetBrains_Mono({
 
 const GamePage = () => {
   return (
-    <div className="bg-white flex flex-col sm:flex-row items-center justify-center relative overflow-hidden min-h-screen">
+    <div className="bg-white flex flex-col sm:flex-row items-center justify-center relative">
       {/* <Sidebar /> */}
         {/* Background NEXUS text with hexagons */}
-        <div className="absolute inset-0 pointer-events-none flex flex-col justify-center items-center overflow-hidden text-[7rem] leading-[0.9] sm:text-[12rem] md:text-[25rem] sm:leading-[0.9] sm:tracking-widest">
+        <div className="absolute inset-0 pointer-events-none flex flex-col justify-center items-center text-[7rem] leading-[0.9] sm:text-[12rem] md:text-[25rem] sm:leading-[0.9] sm:tracking-widest">
           {/* Add the new class here */}
-          <div className={`text-effect-container ${customFont.className} font-bold whitespace-nowrap`}>
+          <div className={`${effectStyles.effectContainer} ${integralCF.className} font-bold h-full flex flex-col justify-between`}
+            style={{
+              WebkitTextStroke: '2px black', 
+            }}
+          >
             <div>NEXUS</div>
             <div>NEXUS</div>
             <div className="lg:hidden">NEXUS</div>
@@ -41,31 +60,6 @@ const GamePage = () => {
             <div className="sm:hidden">NEXUS</div>
             <div className="sm:hidden">NEXUS</div>
           </div>
-        </div>
-        {/* Bottom left hexagons - sm and up only */}
-        <div className="hidden sm:block absolute left-0 bottom-0 pointer-events-none opacity-15">
-          <svg width="300" height="300" viewBox="0 0 300 300" className="text-gray-400">
-            <defs>
-              <pattern id="hexPattern" x="0" y="0" width="80" height="80" patternUnits="userSpaceOnUse">
-                <polygon points="40,10 70,25 70,55 40,70 10,55 10,25" fill="none" stroke="currentColor" strokeWidth="2"/>
-                <polygon points="40,10 70,25 70,55 40,70 10,55 10,25" fill="none" stroke="currentColor" strokeWidth="1" opacity="0.3" transform="translate(40, 40)"/>
-              </pattern>
-            </defs>
-            <rect width="300" height="300" fill="url(#hexPattern)"/>
-          </svg>
-        </div>
-
-        {/* Top right hexagons - sm and up only */}
-        <div className="hidden sm:block absolute right-0 top-0 pointer-events-none opacity-15">
-          <svg width="300" height="300" viewBox="0 0 300 300" className="text-gray-400">
-            <defs>
-              <pattern id="hexPattern2" x="0" y="0" width="80" height="80" patternUnits="userSpaceOnUse">
-                <polygon points="40,10 70,25 70,55 40,70 10,55 10,25" fill="none" stroke="currentColor" strokeWidth="2"/>
-                <polygon points="40,10 70,25 70,55 40,70 10,55 10,25" fill="none" stroke="currentColor" strokeWidth="1" opacity="0.3" transform="translate(40, 40)"/>
-              </pattern>
-            </defs>
-            <rect width="300" height="300" fill="url(#hexPattern2)"/>
-          </svg>
         </div>
 
         <div className="sm:w-[60%] z-10 flex flex-col items-center sm:h-screen justify-center mx-auto"> 
