28ada1ae08de3a68c1f04503106b39515a69aab3
KJJ1305 <jesvinjaisonkurisingal@gmail.com>
2025-10-18 03:58:51 +0530

    login in hero

--- tathva25frontend/src/components/heroPage/navbar.jsx
+++ tathva25frontend/src/components/heroPage/navbar.jsx
@@ -7,19 +7,7 @@ export default function Navbar() {
     <nav
       className={` fixed top-0 left-0 w-full z-50 transition-all duration-300 `}
     >
-      <div className="mr-5   flex justify-between items-center">
-        <div className="ml-auto mt-4">
-          <Image
-            src={logo}
-            alt="Tathva Logo"
-            width={100}
-            height={100}
-            className="h-10 mt-2 w-auto transition-transform duration-300 hover:scale-105"
-            priority
-            quality={90}
-          />
-        </div>
-      </div>
+      
     </nav>
   );
 }
